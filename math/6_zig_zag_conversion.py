72db6b0e07c96f4c1039cccfb5ebf61db014dffd
ryan <ryansoftwareengineer@gmail.com>
2021-03-08 21:08:49 -0800

    6 zig zag

---
+++ math/6_zig_zag_conversion.py
@@ -0,0 +1,53 @@
+'''
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display
+this pattern in a fixed font for better legibility)
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+'''
+
+# create a matrix of rows
+# set direction to increase
+# for every iteration of string s ... append to matrix then either increase or decrease row number
+# when row = 0 or numRows then change directon to increase row
+
+class attempt_one:
+    # fill a matrix then iterate hte matrix in order
+    def convert(self, s: str, numRows: int) -> str:
+        if len(s) < 2:
+            return s
+        row = -1
+        cycle = 2 * numRows - 2
+        matrix = []
+        for i, val in enumerate(s):
+            if i % cycle == 0:
+                row += 1
+                matrix.append([])
+            matrix[row].append(val)
+        row = col = 0
+        result = ''
+        max_col = len(matrix) - 1
+        while len(result) < len(s):
+            # this is bad string concatenation for larger data sets, shoulda appended to list then ''.join(list) at the end
+            result += matrix[row][col]
+            if col > 0 and col < numRows - 1 and len(matrix[row]) > cycle - col:
+                result += matrix[row][cycle - col]
+            row += 1
+            if row > max_col:
+                row = 0
+                col += 1
+            if row == max_col and col > len(matrix[max_col]) - 1:
+                row = 0
+                col += 1
+        return result
--- test.py
+++ test.py
@@ -2,42 +2,23 @@
 this script is just for me to test new things i'm learning
 '''
 
-
-# def perm( arr, l, end, result):
-#     if l == end:
-#         print(arr)
-#         return
-#     i = l
-#     while i <= end:
-#         arr[i], arr[l] = arr[l], arr[i]
-#         perm(arr, l+1, end, result)
-#         arr[i], arr[l] = arr[l], arr[i]
-#         i+=1
 #
-# cand = [2, 5, 7]
-# perm(cand, 0, len(cand)-1, set())
-
-# matrix = []
-# row = [0, 0]
-# for i in range(2):
-#     matrix.append(row)
-# print(matrix)
-# matrix[1][1] = 1
-# print (matrix)
-def factory():
-    values = []
-    def widget(value):
-        values.append(value)
-        return values
-    print(values)
-    return widget
-'''
-widget has it's own state 
-'''
-worker = factory()
-worker(1)
-worker(2)
-print(worker(4))
-print(2%3)
-print(1//2)
-print([4]+ [3])
+# def factory():
+#     values = []
+#     def widget(value):
+#         values.append(value)
+#         return values
+#     print(values)
+#     return widget
+# '''
+# widget has it's own state
+# '''
+# worker = factory()
+# worker(1)
+# worker(2)
+# print(worker(4))
+# print(2%3)
+# print(1//2)
+# print([4]+ [3])
+matrix = [[] for _ in range(4)]
+print (matrix)
